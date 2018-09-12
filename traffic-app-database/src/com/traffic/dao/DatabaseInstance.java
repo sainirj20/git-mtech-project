b72c73cf5952985e594c1480b8c27cbf911bd45f
sainirj20 <saini.rj20@gmail.com>
2018-09-12 23:25:08 +0530

    added property file util class

--- traffic-app-database/src/com/traffic/dao/DatabaseInstance.java
+++ traffic-app-database/src/com/traffic/dao/DatabaseInstance.java
@@ -1,10 +1,9 @@
 package com.traffic.dao;
 
-import java.io.IOException;
-
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.client.MongoDatabase;
+import com.traffic.utils.PropertiesUtil;
 
 final public class DatabaseInstance {
 	private static Object mutex = new Object();
@@ -13,18 +12,13 @@ final public class DatabaseInstance {
 	private static MongoDatabase DATABASE;
 
 	private DatabaseInstance() {
+		System.out.println(PropertiesUtil.getPropertyValue("test"));
 		MongoCredential.createCredential("sampleUser", "myDb", "password".toCharArray());
 		System.out.println("Connected to the database successfully");
 		DATABASE = mongo.getDatabase("myDb");
 	}
 
 	public static MongoDatabase getInstance() {
-		try {
-			String current = new java.io.File(".").getCanonicalPath();
-			System.out.println("Current dir:" + current);
-			System.exit(0);
-		} catch (IOException e) {
-		}
 		if (DATABASE == null) {
 			synchronized (mutex) {
 				new DatabaseInstance();
---
+++ traffic-app-utils/src/com/traffic/utils/PropertiesUtil.java
@@ -0,0 +1,32 @@
+package com.traffic.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesUtil {
+	private static Properties prop = new Properties();
+
+	static {
+		try {
+			prop.load(new FileInputStream("config.properties"));
+			System.out.println("properties loaded");
+		} catch (IOException ex) {
+			try {
+				System.out.println("config.properties file to this location :: " + new File(".").getCanonicalPath());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.exit(0);
+		}
+	}
+
+	private PropertiesUtil() {
+	}
+
+	public static String getPropertyValue(String key) {
+		return prop.getProperty(key);
+	}
+
+}
